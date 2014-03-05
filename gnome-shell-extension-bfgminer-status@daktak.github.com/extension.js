545f2c5e678ee2088b50fbf7495a6556dace68a2
daktak <daktaklakpak@eml.cc>
2014-03-05 14:29:00 +1100

    initial commit

---
+++ gnome-shell-extension-bfgminer-status@daktak.github.com/extension.js
@@ -0,0 +1,78 @@
+/*
+ * config variables
+ * host
+ * port
+ * number of decimals
+ */
+const St = imports.gi.St;
+const Main = imports.ui.main;
+const Tweener = imports.ui.tweener;
+const GLib = imports.gi.GLib;
+const Gio = imports.gi.Gio;
+const GObject = imports.gi.GObject;
+
+
+
+let text, button;
+
+function _hideHello() {
+    Main.uiGroup.remove_actor(text);
+    text = null;
+}
+
+function _getValue() {
+      try {
+      var client = new Gio.SocketClient ();
+      var conn = client.connect_to_host("localhost:4028", null, null);
+
+      //print("Connected to server");
+
+      var output = conn.get_output_stream();
+      var output_stream = new Gio.DataOutputStream.new(output);
+
+      var message = JSON.stringify({ command: 'summary', parameter: '' } );
+
+      output_stream.put_string(message,null);      
+      output.flush(null);
+      
+      var input = conn.get_input_stream();
+      var input_stream = new Gio.DataInputStream.new(input);
+      var data = input_stream.read_upto("\n", 1, null);
+
+      var dataS = data.toString();
+      var data_out = dataS.substr(0,dataS.lastIndexOf('}')+1);
+      var data_json = JSON.parse(data_out);
+      var unit = " MHS";
+
+      var num = parseFloat(data_json["SUMMARY"][0]["MHS av"]);
+      if (num > 1000){
+         num = num / 1000;
+         unit = " GHS";
+      }
+      num = parseFloat(num).toFixed(0);
+
+      } catch (err) {
+      	return err.toString();
+      }
+      return num.toString()+unit;
+}
+
+function init() {
+    button = new St.Bin({ style_class: 'panel-button',
+                          reactive: true,
+                          can_focus: true,
+                          x_fill: true,
+                          y_fill: false,
+                          track_hover: true });
+    let bt = new St.Label({text:_( _getValue() )});
+    button.set_child(bt);
+    button.connect('button-press-event', _getValue);
+}
+
+function enable() {
+    Main.panel._rightBox.insert_child_at_index(button, 0);
+}
+
+function disable() {
+    Main.panel._rightBox.remove_child(button);
+}
